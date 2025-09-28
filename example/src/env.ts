095a6e8dff27b2f3644b995c38033c3bcfa83867
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2025-09-28 15:48:47 +0900

    use t3env

--- example/src/env.ts
+++ example/src/env.ts
@@ -1,13 +1,15 @@
 import * as v from "valibot";
+import { createEnv } from "@t3-oss/env-nextjs";
 
-const EnvSchema = v.object({
-  AUTH_SPOTIFY_CLIENT_ID: v.string(),
-  AUTH_SPOTIFY_CLIENT_SECRET: v.string(),
-  AUTH_SECRET: v.string(),
+export const env = createEnv({
+  server: {
+    AUTH_SPOTIFY_CLIENT_ID: v.string(),
+    AUTH_SPOTIFY_CLIENT_SECRET: v.string(),
+    AUTH_SECRET: v.string(),
+  },
+  runtimeEnv: {
+    AUTH_SPOTIFY_CLIENT_ID: process.env.AUTH_SPOTIFY_CLIENT_ID,
+    AUTH_SPOTIFY_CLIENT_SECRET: process.env.AUTH_SPOTIFY_CLIENT_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  },
 });
-
-export const env = v.parse(EnvSchema, {
-  AUTH_SPOTIFY_CLIENT_ID: process.env.AUTH_SPOTIFY_CLIENT_ID,
-  AUTH_SPOTIFY_CLIENT_SECRET: process.env.AUTH_SPOTIFY_CLIENT_SECRET,
-  AUTH_SECRET: process.env.AUTH_SECRET,
-} satisfies Record<keyof typeof EnvSchema.entries, unknown>);
